adcc61c359f90d48b27974670565efac40028c27
limbing <liislambing@gmail.com>
2024-10-10 11:34:21 +0300

    töö on tehtud

--- praktiline_too_10.10.py
+++ praktiline_too_10.10.py
@@ -11,3 +11,8 @@ b = float(input("Sisesta oma b kaateti pikkus: "))
 
 # meie ülesandeks on leida hüpoteenus c, kolmnurga ümbermõõt ja pindala NB(vastused peavad olema ümardatud sajandikeni)
 c = math.sqrt((a ** 2) + (b ** 2))
+print(f"Hüpoteenus c pikkus on: {round(c, 2)}")
+p = (a + b + c)
+print(f"kolmnurga pindala on: {round(p, 2)}")
+s = ((a * b) / 2)
+print(f"Kolmnurga pindala on: {round(s, 2)}")
